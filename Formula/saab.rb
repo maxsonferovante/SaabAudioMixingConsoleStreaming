c3c5659d7ffdd3d0d7662516b19f562387435818
maxsonferovante <maxsonferovante@gmail.com>
2026-08-20 19:31:48 -0300

    chore: bump project version to 0.4.2 and update Homebrew formula to 0.4.1

--- Formula/saab.rb
+++ Formula/saab.rb
@@ -1,15 +1,15 @@
 class Saab < Formula
   desc "Ultra-low-latency distributed audio mixing console CLI for macOS and Android"
   homepage "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.0/saab-v0.4.0-macos-arm64.tar.gz"
-    sha256 "e7d2331c9af44d3c6220e3cffa7a7437bbd55a4ffc0ee175b12dd9d7ce5d865d"
+    url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.1/saab-v0.4.1-macos-arm64.tar.gz"
+    sha256 "e079daded459a4908cf084bddc43751edff423bc5c6c138d11caf56f349c4651"
   else
-    url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.0/saab-v0.4.0-macos-x86_64.tar.gz"
-    sha256 "d59e0f8368ea905973a6d279e5401744ad3fd71b8e253651f3984fc73213f68f"
+    url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.1/saab-v0.4.1-macos-x86_64.tar.gz"
+    sha256 "030597975027ed29b619546182038b50a2cada4e9c5661ad947987b5d72d7cec"
   end
 
   def install
