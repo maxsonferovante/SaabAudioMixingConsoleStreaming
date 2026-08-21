31a006220a72a6d57e325e876136c884e55a134f
maxsonferovante <maxsonferovante@gmail.com>
2026-08-20 21:42:41 -0300

    chore: revert version to 0.4.3 and update checksums

--- Formula/saab.rb
+++ Formula/saab.rb
@@ -1,15 +1,15 @@
 class Saab < Formula
   desc "Ultra-low-latency distributed audio mixing console CLI for macOS and Android"
   homepage "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming"
-  version "0.4.4"
+  version "0.4.3"
   license "MIT"
 
   if Hardware::CPU.arm?
     url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.3/saab-v0.4.3-macos-arm64.tar.gz"
-    sha256 "030597975027ed29b619546182038b50a2cada4e9c5661ad947987b5d72d7202"
+    sha256 "60268e3a41e2e30223b75cb700b0d686b9e73bbfd8aa0704df2fa7916762988a"
   else
     url "https://github.com/maxsonferovante/SaabAudioMixingConsoleStreaming/releases/download/v0.4.3/saab-v0.4.3-macos-x86_64.tar.gz"
-    sha256 "030597975027ed29b619546182038b50a2cada4e9c5661ad947987b5d72d7c98"
+    sha256 "8f71e048ed128efc4f569777434f0ab47b1c603b3c5abcdbaea8962bb74f1efa"
   end
 
   def install
